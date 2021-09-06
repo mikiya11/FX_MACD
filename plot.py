b49729085ce3fba367b67c743eb381d3a1fd7683
mikiya11 <mai.m2000@outlook.jp>
2021-09-06 22:08:57 +0900

    check model

---
+++ plot.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import japanize_matplotlib
+
+data = pd.read_csv('./data/train_log.csv',encoding = 'UTF8')
+
+data = data.iloc[:, 2:]
+
+loss = data.iloc[:, 0]
+
+acc = data.iloc[:, 1]
+
+vali_loss = data.iloc[:, 2]
+
+vali_acc = data.iloc[:, 3]
+
+fig = plt.figure()
+ax1 = fig.add_subplot(2, 2, 1)
+ax2 = fig.add_subplot(2, 2, 2)
+ax3 = fig.add_subplot(2, 2, 3)
+ax4 = fig.add_subplot(2, 2, 4)
+
+l1,l2,l3,l4 = "loss","acc","vali_loss","vali_acc" 
+
+ax1.plot(loss, label=l1)
+ax2.plot(acc, label=l2)
+ax3.plot(vali_loss, label=l3)
+ax4.plot(vali_acc, label=l4)
+
+ax1.legend(loc = 'upper right') #凡例
+ax2.legend(loc = 'upper right') #凡例
+ax3.legend(loc = 'upper right') #凡例
+ax4.legend(loc = 'upper right') #凡例
+fig.tight_layout()              #レイアウトの設定
+plt.show()
