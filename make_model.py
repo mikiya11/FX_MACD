fdace2d9a601825fbb5f11bc7eb59a7ebeea4468
mikiya11 <mai.m2000@outlook.jp>
2021-09-07 03:38:29 +0900

    make model

--- make_model.py
+++ make_model.py
@@ -1,13 +1,13 @@
 import pandas as pd
 import numpy as np
 from statistics import mean,stdev
-from torch.utils.data import DataLoader
-import torch.nn as nn
-from torchvision import models
 import os
 #「学習」
 import torch
 import torch.optim as optimizers
+from torch.utils.data import DataLoader
+import torch.nn as nn
+from torchvision import models
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 import copy
 
